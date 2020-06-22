b0ef2a5094d71046ece078c2986fb23fec1a1d02
Luciana Barbosa <luciheit@gmail.com>
2020-06-22 15:37:23 -0300

    permiti que o script rode apenas se o usuário digitar números nos campos de nota

--- exercicios/exercicio-alunas/lucianahb/script.js
+++ exercicios/exercicio-alunas/lucianahb/script.js
@@ -3,20 +3,25 @@ let nota1 = parseFloat(prompt('Digite a nota do 1º semestre:').replace(',','.')
 let nota2 = parseFloat(prompt('Digite a nota do 2º semestre:').replace(',','.'));
 let nota3 = parseFloat(prompt('Digite a nota do 3º semestre:').replace(',','.'));
 let media = 7;
-let mediaAluno = (nota1 + nota2 + nota3) / 3;
-let alunoAprovado = mediaAluno >= media;
-let alunoReprovado = mediaAluno < media;
-// console.log(nota1, nota2, nota3, mediaAluno);
+let notasValidas = nota1 !== NaN && nota2 !== NaN && nota3 !== NaN;
+let mediaAluno;
+let alunoAprovado;
+let alunoReprovado;
 
-
-if (alunoAprovado) {
-  alert (`Parabéns, ${nome}, você foi passou de ano com média ${mediaAluno.toFixed(0)}, aproveite suas férias!`);
-} else if (alunoReprovado){
-    alert (`Que pena, ${nome}, sua média é ${mediaAluno.toFixed(0)} e você não obteve aprovação.`);
-    let pergunta = confirm('Gostaria de fazer recuperação?');
-    if (pergunta) {
-      alert (`Ótimo, as aulas de recuperação começam semana que vem, ${nome}!`);
-    } else {
-        alert (`Que pena, te vejo ano que vem, ${nome}`);
-    }
+if (notasValidas) {
+  mediaAluno = (nota1 + nota2 + nota3) / 3;
+  alunoAprovado = mediaAluno >= media;
+  alunoReprovado = mediaAluno < media;
+  
+  if (alunoAprovado) {
+    alert (`Parabéns, ${nome}, você foi passou de ano com média ${mediaAluno.toFixed(0)}, aproveite suas férias!`);
+  } else if (alunoReprovado){
+      alert (`Que pena, ${nome}, sua média é ${mediaAluno.toFixed(0)} e você não obteve aprovação.`);
+      let pergunta = confirm('Gostaria de fazer recuperação?');
+      if (pergunta) {
+        alert (`Ótimo, as aulas de recuperação começam semana que vem, ${nome}!`);
+      } else {
+          alert (`Que pena, te vejo ano que vem, ${nome}`);
+      }
+  }
 }
