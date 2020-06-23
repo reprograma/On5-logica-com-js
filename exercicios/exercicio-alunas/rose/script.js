0b4ddba676c540cb1d7611e41b0b157f80ff2f33
rose <rosefelix99@gmail.com>
2020-06-23 19:06:45 -0300

    Adicionado nome do aluno e mensagem caso resultado reprovado

--- exercicios/exercicio-alunas/rose/script.js
+++ exercicios/exercicio-alunas/rose/script.js
@@ -1,3 +1,4 @@
+let nome = prompt("Qual é o seu nome?");
 let n1 = prompt("Digite nota 01: ");
 let n2 = prompt("Digite nota 02: ");
 let n3 = prompt("Digite nota 03: ");
@@ -8,8 +9,16 @@ let media = (parseInt(n1)+parseInt(n2)+parseInt(n3))/3;
 
 console.log(media);
 
+console.log(nome);
+
 if (media >= 7){
-    alert("Parabéns, você foi aprovado, aproveite suas férias!");
+    alert(`Parabéns ${nome}, você foi aprovado(a), aproveite suas férias!`);
 }else{
-    alert("Que pena, você foi reprovado.");
+    let recuperacao = confirm(`${nome}, você gostaria de fazer as aulas de recuperação para tentar melhorar a sua nota?`);
+
+    if (recuperacao === true){
+        alert("Ótimo, as aulas de recuperação começam semana que vem!")
+    }else{
+        alert("Que pena, te vejo ano que vem.")
+    }
 }
