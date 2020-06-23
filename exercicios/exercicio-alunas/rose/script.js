06cf93a8ff197151b46a12855c4281900615719d
rose <rosefelix99@gmail.com>
2020-06-23 20:20:56 -0300

    Arredondando a média final do Aluno

--- exercicios/exercicio-alunas/rose/script.js
+++ exercicios/exercicio-alunas/rose/script.js
@@ -3,22 +3,36 @@ let n1 = prompt("Digite nota 01: ");
 let n2 = prompt("Digite nota 02: ");
 let n3 = prompt("Digite nota 03: ");
 
-console.log(n1, n2, n3);
+// ## Conferindo dados inseridos pelo usuário##
+console.log("Nome: " + nome);
+console.log("Notas: " + n1, n2, n3);
 
+// ## Cálculando Média das notas ##
 let media = (parseInt(n1)+parseInt(n2)+parseInt(n3))/3;
+console.log("Média: " + media);
 
-console.log(media);
+// ## Pegando a parte quebrada da média para usar na regra de arredondamento ##
+let nquebrado = media %1; 
+console.log("Numero quebrado: " + nquebrado);
 
-console.log(nome);
+// ## Quando a parte quebrada da média for maior que 0.5 será arredondado para mais e o contrário para menos ##
+let fmedia; //variável para guardar a média formatada.
+if (nquebrado > 0.5){
+    fmedia = Math.ceil(media);
+}else{
+   fmedia = Math.floor(media);
+}
+console.log("Media Formatada " + fmedia);
 
-if (media >= 7){
+// ## Verificando se houve aprovação ##
+if (fmedia >= 7){
     alert(`Parabéns ${nome}, você foi aprovado(a), aproveite suas férias!`);
 }else{
     let recuperacao = confirm(`${nome}, você gostaria de fazer as aulas de recuperação para tentar melhorar a sua nota?`);
 
     if (recuperacao === true){
-        alert("Ótimo, as aulas de recuperação começam semana que vem!")
+        alert("Ótimo, as aulas de recuperação começam semana que vem!");
     }else{
-        alert("Que pena, te vejo ano que vem.")
+        alert("Que pena, te vejo ano que vem.");
     }
 }
