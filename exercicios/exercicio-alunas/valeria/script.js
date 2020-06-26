de9f91ecaaa0df285a86bb4620e135cf5a35bad6
ValériadeJesus <valeriaNjesus@gmail.com>
2020-06-26 16:47:42 -0400

    Ajustes no calcule a média final

--- exercicios/exercicio-alunas/valeria/script.js
+++ exercicios/exercicio-alunas/valeria/script.js
@@ -1,24 +1,16 @@
 let nomeAluno = String(prompt('Digite seu nome:'));
-    
-
-
 let notaUm = Number(prompt('Digite sua nota do primeiro trimestre:'));
-let notaDois = Number(prompt('Agora digite a nota do segundo trimestre:'));
+let notaDois = Number(prompt('Digite a nota do segundo trimestre:'));
 let notaTres = Number(prompt('Digite a nota do terceiro trimestre:'));
 
 let mediaAluno = Math.round((notaUm + notaDois + notaTres) / 3);
 let mensagem = String;
 
-
-
-
-
-
 if (mediaAluno >= 7) {
-    
-    alert('Parabéns você foi aprovado, boas férias!');
 
- } else  {
-    mensagem = ('Infelizmente, você não foi aprovado.');
-    alert(mensagem);
- }
+   alert('Parabéns você foi aprovado, boas férias!');
+
+} else {
+   mensagem = ('Infelizmente, você não foi aprovado.');
+   alert(mensagem);
+}
