5cb1fde03095fe3e999ae74e9c8d0b376e3263ea
vaosorio <vaosorio.dev@gmail.com>
2020-06-26 00:56:05 -0300

    Arruando os parametros de arrendodamento de notas

--- exercicios/exercicio-alunas/vanessa-osorio/script.js
+++ exercicios/exercicio-alunas/vanessa-osorio/script.js
@@ -6,12 +6,12 @@ let nota2 = Number(prompt('Digite a nota 2'));
 let nota3 = Number(prompt('Digite a nota 3'));
 let media = (Number(nota1) + Number(nota2) + Number(nota3)) / 3;
 
-
+console.log(media);
 if (media >= 6.6 || media >= 6.7) {
-    media = Math.floor(media).toFixed(1);
+    media = Math.ceil(media).toFixed(1);
     alert(`Parabéns, ${nome} foi aprovado(a) com média ${media} , aproveite suas férias!`);
 } else if (media <= 6.8 || media <= 6.9) {
-    media = Math.ceil(media).toFixed(1);
+    media = Math.floor(media).toFixed(1);
     alert(`Que pena ${nome}, sua média é ${media} e você foi reprovado(a).`);
     let recuperar = confirm(`${nome}, você gostaria de fazer aulas de recuperação para tentar melhorar a nota e passar de ano.`);
 
